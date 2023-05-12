5147fad60c44acbdef545169715d386318d9759a
Paul Preda <ppd1512@gmail.com>
2023-05-12 14:11:07 +0100

    fixed the problem with not being able to add shows to the user

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const Show = require('./Show')
 const User = require('./User')
 
-Show.belongsTo(User)
-User.hasMany(Show)
+Show.belongsToMany(User, {through: "User_Shows"})
+User.belongsToMany(Show, {through: "User_Shows"})
 
 module.exports = {
     Show, 
